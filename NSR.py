a5f3c9fee1263d3f256d28533b4b53cb7550ea1c
GUTIERREZ-CANALES Fernando <fernando.canales@obspm.fr>
2022-11-29 10:14:51 +0100

    Added the reshape to the mask in NSR.py

--- NSR.py
+++ NSR.py
@@ -1,25 +1,28 @@
 import numpy as np
 
+
 # Let's calculate  the critical SPR
 def spr_crit(dback, nsr, td, ntr):
     term1 = 7.1 / dback
     term2 = min(nsr) / np.sqrt(td * ntr)
     return term1 * term2
 
+
 # Let's calculate the NSR of the target
 def NSRn(sb, sd, sq, ft, fc):
     n = np.sqrt(ft + fc + sb + sd ** 2 + sq ** 2) / ft
     return n
 
+
 # Let's go Aussie mode, keegan, yo!
 def nsr_AGG(x, y, sb, sd, sq):
     n = []
-    for i in range(1, len(x)+1):
-        n.append(np.sqrt(np.sum(x[:i] + y[:i] + sb + sd ** 2 + sq ** 2))/np.sum(x[:i]))
+    for i in range(1, len(x) + 1):
+        n.append(np.sqrt(np.sum(x[:i] + y[:i] + sb + sd ** 2 + sq ** 2)) / np.sum(x[:i]))
     return n
 
-def aperture(ft, fc, sb, sd, sq):
 
+def aperture(ft, fc, sb, sd, sq):
     # First we compute the NSR of the system
     nsr = np.sqrt(ft + fc + sb + sd ** 2 + sq ** 2) / ft
 
@@ -45,7 +48,7 @@ def aperture(ft, fc, sb, sd, sq):
     # We compute the nsr_agg over 1h
     nsr1h = ((10 ** 6) / (12 * np.sqrt(24))) * n
 
-    # First we create a vector wiht only zeroes
+    # First we create a vector with only zeroes
     w = np.zeros(36)
 
     # Then we create a vector with just the amount of indexes of the mask
@@ -53,8 +56,25 @@ def aperture(ft, fc, sb, sd, sq):
 
     # Then we create our mask, we show the index where the mask vector has a value of 1
     w[mask] = 1
+    w = w.reshape((6, 6))
 
     return nsr1h, w
+
+
+def extended_binary_mask(mask, W):
+    ny, nx = mask.shape
+    maske = np.zeros((ny, nx))
+    maske[:, :] = mask[:, :]
+    for j in range(ny):
+        for i in range(nx):
+            if mask[j, i] > 1.0 - 1e-5:
+                for k in range(-W, W + 1):
+                    if (j + k >= 0) & (j + k < ny):
+                        for m in range(-W, W + 1):
+                            if (i + m >= 0) & (i + m < nx):
+                                maske[j + k, i + m] = 1
+    return maske
+
 
 # Let's try the code!
 if __name__ == '__main__':
