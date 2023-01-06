f2e01ba672cd4f69a431bdacd317d2a1931ede53
GUTIERREZ-CANALES Fernando <fernando.canales@obspm.fr>
2023-01-06 13:55:32 +0100

    Implemented more plots

--- plotting_efficiency.py
+++ plotting_efficiency.py
@@ -6,6 +6,7 @@ from matplotlib.ticker import PercentFormatter
 data = np.load('targets_P5.npy')
 data_sec = np.load('targets_P5_contaminant.npy')
 data_ext = np.load('targets_P5_extended.npy')
+data_bray = np.load('targets_P5_bray.npy')
 
 # We obtain the magnitude of all the targets
 mag = data[:, 1]
@@ -19,7 +20,7 @@ mask_p5 = (mag >= 10.66) & (mag <= 12.66)
 # Now we apply the mask for getting the magnitude range
 mag_p5 = mag[mask_p5]
 
-# Now we apply the mask again to estimate the number of N_bad in the P5 sample magnitude range
+# Now we apply the mask again to estimate the number of N_bad in the P5 sample magnitude range given the nominal mask
 n_bad_p5 = n_bad[mask_p5]
 
 # Now we make percentage histogram as the one presented by Marchiori
@@ -37,10 +38,12 @@ plt.show()
 # We get the eta_t and eta_c
 eta_t = data[:, 12]
 eta_c = data_sec[:, 6]
+nsr1h = data[:, 7]
 
 # Now we get the eta_t and eta_c only for the stars of the P5 sample
 eta_t_P5 = eta_t[mask_p5]
 eta_c_P5 = eta_c[mask_p5]
+nsr1h_p5 = nsr1h[mask_p5]
 
 # Now we create a mask to estimate the number of false positives detected by Marchiori's secondary mask. This means, the
 # number of cases where eta_c > eta_t given that eta_t > 7.1
@@ -107,4 +110,22 @@ print("The efficiency of Extended mask's method is:", "%.4f" % (N_bad_extended /
 plt.hist(mag_p5[N_bad], bins=[10, 11, 12, 13], edgecolor='black', label='Nominal Mask', alpha=0.5)
 plt.hist(mag_p5[N_bad_extended], bins=[10, 11, 12, 13], edgecolor='black', label='Secondary Mask', alpha=0.5)
 plt.legend()
+plt.show()
+
+# Now bray's assumption
+n_bad_bray = data_bray[:, 4]
+nsr1h_bray = data_bray[:, 3]
+
+n_bad_bray_p5 = n_bad_bray[mask_p5]
+nsr1h_bray_p5 = nsr1h_bray[mask_p5]
+
+# Now we make percentage histogram as the one presented by Marchiori
+bins = [-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5]
+plt.hist(n_bad_p5, bins=bins, weights=[1 / len(n_bad_p5)] * len(n_bad_p5), edgecolor='black', rwidth=0.8, label='Marchiori', alpha=0.5)
+plt.hist(n_bad_bray_p5, bins=bins, weights=[1 / len(n_bad_bray_p5)] * len(n_bad_bray_p5), edgecolor='black', rwidth=0.8, label='Bray', alpha=0.5)
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.legend()
+plt.show()
+
+plt.plot(nsr1h_p5)
 plt.show()
