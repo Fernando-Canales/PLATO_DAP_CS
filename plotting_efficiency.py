e7839d4150dfebe286cebdff13c2598270fbdea9
GUTIERREZ-CANALES Fernando <fernando.canales@obspm.fr>
2023-01-11 15:47:26 +0100

    Simplified the computation of the efficiencies

--- plotting_efficiency.py
+++ plotting_efficiency.py
@@ -33,93 +33,52 @@ plt.show()
 # ratio between the total number of false positives detected by the nominal mask (i.e N_bad) over the total number of
 # false positives detected by the secondary mask (N_bad_sec) and by the Extended mask (N_bad_ext). This means, the
 # efficiency of Marchiori's method is just N_bad_sec / N_bad, while the efficiency of the Extended mask would be
-# N_bad_ext / N_bad.
+# N_bad_ext / N_bad. The thing to do is therefore to write an expression for obtaining each of these three quantities.
+# In our code, we are assuming (and that is quite a strong assumption!) that all contaminant stars for every target are
+# eclipsing binaries with the same transit depth for every of them. In such a case, the total number of false positives
+# detectable for the nominal mask is just the number of contaminant stars that could generate a detectable signal on the
+# nominal mask (i.e \eta_t > 7.1). On the other hand, the number of false positives that can be spotted by the secondary
+# or Extended masks is the number of stars that can produce a detectable signal on the nominal mask as well
+# (eta_t > 7.1) given that they are detectable on their respective masks (eta_c > 7.1 and eta_ext > 7.1) and
+# (delta_obs_c > delta_obs_t delta_obs_ext > delta_obs_t). (This can be discussed as well)
 
-#
-
-# The thing to do is therefore to write an expression for obtaining each of these three quantities. In our code, we are
-# assuming (and that is quite a strong assumption!) that all contaminant stars for every target are eclipsing binaries
-# with the same transit depth for every of them. In such a case, the total number of false positives detectable for
-# the nominal mask is just the number of contaminant stars that could generate a detectable signal on the nominal mask
-# (i.e \eta_t > 7.1). On the other hand, the number of false positives that can be spotted by the secondary or Extended
-# masks is the number of stars that can produce a detectable signal on the nominal mask as well (eta_t > 7.1) given
-# that they are detectable on their respective masks (eta_c > 7.1 and eta_ext > 7.1) and (delta_obs_c > delta_obs_t
-# delta_obs_ext > delta_obs_t). (This can be discussed as well)
-
-# We get the eta_t and eta_c
+# Now we get all the etas and delta_obs
 eta_t = data[:, 12]
+delta_obs_t = data[:, 13]
 eta_c = data_sec[:, 6]
+delta_obs_c = data_sec[:, 7]
+eta_ext = data_ext[:, 7]
+delta_obs_ext = data_ext[:, 8]
 nsr1h = data[:, 7]
 
-# Now we get the eta_t and eta_c only for the stars of the P5 sample
+# Now we get the etas and delta_obs for the P5 sample
 eta_t_P5 = eta_t[mask_p5]
+delta_obs_t_P5 = delta_obs_t[mask_p5]
 eta_c_P5 = eta_c[mask_p5]
+delta_obs_c_P5 = delta_obs_c[mask_p5]
+eta_ext_P5 = eta_ext[mask_p5]
+delta_obs_ext_P5 = delta_obs_ext[mask_p5]
 nsr1h_p5 = nsr1h[mask_p5]
 
-# Now we create a mask to estimate the number of false positives detected by Marchiori's secondary mask. This means, the
-# number of cases where eta_c > eta_t given that eta_t > 7.1
-mask_marchiori = (eta_c_P5 > eta_t_P5) & (eta_t_P5 > 7.1)
-
-# Now we create a mask to estimate the total number of false positives of the P5 sample. This means, the number of cases
-# where eta_t > 7.1
-mask_total_false = (eta_t_P5 > 7.1)
-
-# Now we can estimate the number of false positives detected by Marchiori's secondary mask
-false_positives_marchiori = mask_marchiori.sum()
+# Now we estimate the number of false positives detected by the secondary mask (Marchiori's approach)
+false_positives_marchiori = ((eta_t_P5 > 7.1) & (delta_obs_c_P5 > delta_obs_t_P5) & (eta_c_P5 > 7.1)).sum()
 
 # Now we can estimate the total the number of false positives
-false_positives_p5 = mask_total_false.sum()
+false_positives_p5 = (eta_t_P5 > 7.1).sum()
 
 # Now we obtain the efficiency of Marchiori's method
-# print(((eta_c_P5 > eta_t_P5) & (eta_t_P5 > 7.1)).sum() / (eta_t_P5 > 7.1).sum())
 print("The efficiency of Marchiori's method is:", "%.4f" % (false_positives_marchiori / false_positives_p5))
 
-# Now we can plot a histogram that contains our estimations of the total number of false positives and the number of
-# false positives detected by Marchiori's method for given magnitude bins
-plt.hist(mag_p5[mask_total_false], bins=[10, 11, 12, 13], edgecolor='black', label='Nominal Mask', alpha=0.5)
-plt.hist(mag_p5[mask_marchiori], bins=[10, 11, 12, 13], edgecolor='black', label='Secondary Mask', alpha=0.5)
-plt.legend()
-plt.show()
 
-# Now we compute the efficiency of the extended mask method. For doing that, we compute the number of cases where the
-# extended mask is able to detect a false positive over the total number of false positives detected by the nominal
-# mask.
-
-sprk_ext = data_ext[:, 5]
-spr_crit_ex = data_ext[:, 6]
-sprk = data[:, 10]
-spr_crit = data[:, 9]
-
-# The efficiency of the extended mask is this the ratio between the  number of contaminants stars for which
-# (sprk_ext > spr_crit_ex) and (sprk > spr_crit) and (sprk_ext>sprk) and N_bad  (i.e. the number of contaminants for
-# which (sprk > spr_crit)
-
-# So, I have to count the  number of contaminants stars for which the condition above is verified and store this number
-
-# I will use the mask again
-sprk_ext_P5 = sprk_ext[mask_p5]
-sprk_crit_ext_P5 = spr_crit_ex[mask_p5]
-sprk_P5 = sprk[mask_p5]
-spr_crit_P5 = spr_crit[mask_p5]
-
-# I will obtain the efficiency
-mask_extended = (sprk_ext_P5 > sprk_crit_ext_P5) & (sprk_P5 > spr_crit_P5) & (sprk_ext_P5 > sprk_P5)
-
-mask_nominal = (sprk_P5 > spr_crit_P5)
-
-# Now we compute the number of N_bad_ext
-N_bad_extended = mask_extended.sum()
-
-# Now we compute number of N_bad
-N_bad = mask_nominal.sum()
+# Now we estimate the number of false positives detected by the Extended mask
+false_positives_extended = ((eta_t_P5 > 7.1) & (delta_obs_ext_P5 > delta_obs_t_P5) & (eta_ext_P5 > 7.1)).sum()
 
 # Now we obtain the efficiency of the extended mask
-print("The efficiency of Extended mask's method is:", "%.4f" % (N_bad_extended / N_bad))
+print("The efficiency of Extended mask's method is:", "%.4f" % (false_positives_extended / false_positives_p5))
 
-# Now we can plot a histogram that contains our estimations of the total number of false positives and the number of
-# false positives detected by Extended mask's method for given magnitude bins
-plt.hist(mag_p5[N_bad], bins=[10, 11, 12, 13], edgecolor='black', label='Nominal Mask', alpha=0.5)
-plt.hist(mag_p5[N_bad_extended], bins=[10, 11, 12, 13], edgecolor='black', label='Secondary Mask', alpha=0.5)
+
+plt.plot(mag_p5,   label='Nominal Mask', alpha=0.5)
+plt.plot(mag_p5, label='Secondary Mask', alpha=0.5)
 plt.legend()
 plt.show()
 
@@ -129,14 +88,3 @@ nsr1h_bray = data_bray[:, 3]
 
 n_bad_bray_p5 = n_bad_bray[mask_p5]
 nsr1h_bray_p5 = nsr1h_bray[mask_p5]
-
-# Now we make percentage histogram as the one presented by Marchiori
-bins = [10, 11, 12, 13]
-plt.hist(mag_p5[nsr1h_bray_p5], bins=bins, edgecolor='black', rwidth=0.8, label='Marchiori', alpha=0.5)
-#plt.hist(mag_p5, bins=bins, edgecolor='black', rwidth=0.8, label='Bray', alpha=0.5)
-plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
-plt.legend()
-plt.show()
-
-plt.plot(nsr1h_p5)
-plt.show()
