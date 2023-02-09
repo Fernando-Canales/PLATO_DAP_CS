3ba4ad421ab650ebf67ed90f4ff265f0b88bd3f9
GUTIERREZ-CANALES Fernando <fernando.canales@obspm.fr>
2023-02-09 14:27:17 +0100

    Replace imagette.py

--- imagette.py
+++ imagette.py
@@ -103,27 +103,29 @@ def ran_unique_int(n, interval):
 
 
 # Let's define a function that computes the COB as well as its significance and its associated error
-def centroid_shift(w, Ik, I, sprk, dback, sb, sd, sq, td, ntr):
+def centroid_shift(w, Ik, I_t, I_contaminants, sprk, dback, sb, sd, sq, td, ntr):
     """"
     Computes the COB and COB shift for the full image (Target + Contaminants) as well as the COB shift error
     and COB shift significance.d
     """
+    # First we define our intensity variable as I_tot
+    I_tot = I_t + I_contaminants
     # First we define the abscissa of each pixel in the array with the target and all contaminants
-    x = np.arange(0, I.shape[1]) + 0.5
+    x = np.arange(0, I_tot.shape[1]) + 0.5
     # Second we define the ordinate of each pixel in the array with the target and all contaminants
-    y = np.arange(0, I.shape[0]) + 0.5
+    y = np.arange(0, I_tot.shape[0]) + 0.5
     # Now we make a grid out of them
     x, y = np.meshgrid(x, y)
     # Now we define the total flux
-    f_tot = np.sum(I * w)
+    f_tot = np.sum(I_tot * w)
     # Now we define the COB on the X-direction
-    c_x = np.sum(I * x * w) / f_tot
+    c_x = np.sum(I_tot * x * w) / f_tot
     # Now we define the COB on the Y-direction
-    c_y = np.sum(I * y * w) / f_tot
+    c_y = np.sum(I_tot * y * w) / f_tot
     # Now we define the Gamma factor along the X-direction
-    gamma_x = ((np.sum(Ik * x * w) / f_tot) - c_x * sprk)
+    gamma_x = (np.sum(Ik * x * w) / f_tot) - c_x * sprk
     # Now we define the Gamma factor along the Y-direction
-    gamma_y = ((np.sum(Ik * y * w) / f_tot) - c_y * sprk)
+    gamma_y = (np.sum(Ik * y * w) / f_tot) - c_y * sprk
 
     # Now we define the total gamma factor
     gamma = np.sqrt(gamma_x ** 2 + gamma_y ** 2)
@@ -131,33 +133,33 @@ def centroid_shift(w, Ik, I, sprk, dback, sb, sd, sq, td, ntr):
     # Now we make sure to deal with the correct units for the C.O.B shift (no ppm)
     Dback = dback * 10 ** -6
 
+    # Now we define the lambda factor
     l = Dback / (1 - Dback * sprk)
 
     # Now we define the centroid shift along the X-direction
-    cs_x = l * gamma_x
+    #cs_x = l * gamma_x
     # Now we define the centroid shift along the Y-direction
-    cs_y = l * gamma_y
+    #cs_y = l * gamma_y
 
     # Then we define the absolute centroid shift
-    abs_cob = l * gamma
+    abs_cs = l * gamma
 
     # In order to compute the error associated with the shift, we have to compute the variance of Iij as follows
-    var_delta = np.mean(I) + sb + sd ** 2 * sq ** 2
+    var_delta = np.mean(I_tot) + sb + sd ** 2 * sq ** 2
 
-    # Now we compute the centroid error along the X-direction
-    sigma_x = (np.sum((x ** 2) * w * var_delta) / (f_tot ** 2)) + ((c_x ** 2) * (np.sum(w * var_delta) / (f_tot ** 2)))
-    # Now we compute the centroid error along the Y-direction
-    sigma_y = (np.sum((y ** 2) * w * var_delta) / (f_tot ** 2)) + ((c_y ** 2) * (np.sum(w * var_delta) / (f_tot ** 2)))
+    # Now we compute the centroid shift error along the X-direction
+    sigma_x = (np.sum((x ** 2) * w * var_delta) / f_tot ** 2) + ((c_x ** 2) * (np.sum(w * var_delta) / f_tot ** 2))
+    # Now we compute the centroid shift error along the Y-direction
+    sigma_y = (np.sum((y ** 2) * w * var_delta) / f_tot ** 2) + ((c_y ** 2) * (np.sum(w * var_delta) / f_tot ** 2))
 
     # Now we compute the error associated with the absolute centroid shift
-    sigma_cs = (1 / abs_cob) * np.sqrt((cs_x ** 2) * (sigma_x ** 2) + (cs_y ** 2) + (sigma_y ** 2))
-    sigma_cs_last = (np.sqrt(2) / gamma) * np.sqrt((gamma_x ** 2 * sigma_x ** 2) + (gamma_y ** 2 * sigma_y ** 2))
+    #sigma_cs = (np.sqrt(2) / abs_cs) * np.sqrt((cs_x ** 2) * (sigma_x ** 2) + (cs_y ** 2) + (sigma_y ** 2))
+    sigma_cs = (np.sqrt(2) / gamma) * np.sqrt((gamma_x ** 2) * (sigma_x ** 2) + (gamma_y ** 2) * (sigma_y ** 2))
     # Now we average the error over 1 hour and 24 cameras
     sigma_1_24 = sigma_cs / (12 * np.sqrt(24))
-    sigma_1_24_last = sigma_cs_last / (12 * np.sqrt(24))
     # Now we compute the statistical significance of the centroid shift
-    eta_cob = abs_cob * np.sqrt(td * ntr) / sigma_1_24
-    return eta_cob, sigma_1_24, abs_cob, sigma_1_24_last
+    eta_cob = l * gamma * np.sqrt(td * ntr) / sigma_1_24
+    return eta_cob, sigma_1_24, abs_cs
 
 
 # This function plots the imagette and the PSF
