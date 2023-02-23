12b7b62e25135d78ecdc7522df2d0461ee45154d
GUTIERREZ-CANALES Fernando <fernando.canales@obspm.fr>
2023-02-23 19:57:50 +0100

    Replace plotting_efficiency.py, include the plots for the other extended and secondary masks (extra pixels)

--- plotting_efficiency.py
+++ plotting_efficiency.py
@@ -11,6 +11,8 @@ Pmax = 13
 binsize = 0.5
 nP = int((Pmax - Pmin) / binsize + 1)
 fsize = 14
+flux_trsh = 7.1
+cob_trsh = 3
 
 # We load the npy files with all the metrics of the nominal and secondary and extended masks
 #data_mag = np.load('SFP_DR3_20220831.npy')
@@ -55,23 +57,58 @@ eta_t = data[:, 12]
 delta_obs_t = data[:, 13]
 eta_c = data_sec[:, 6]
 delta_obs_c = data_sec[:, 7]
+eta_c_2 = data_sec[:, 15]
+delta_obs_c_2 = data_sec[:, 16]
 eta_ext = data_ext[:, 7]
 delta_obs_ext = data_ext[:, 8]
+eta_ext_2 = data_ext[:, 15]
+delta_obs_ext_2 = data_ext[:, 16]
+eta_ext_3 = data_ext[:, 23]
+delta_obs_ext_3 = data_ext[:, 24]
 nsr1h = data[:, 7]
 nsr1h_sec = data_sec[:, 4]
+nsr1h_sec_2 = data_sec[:, 13]
 nsr1h_ext = data_ext[:, 4]
+nsr1h_ext_2 = data_ext[:, 14]
+nsr1h_ext_3 = data_ext[:, 22]
 n_bad_bray = data_bray[:, 4]
 nsr1h_bray = data_bray[:, 3]
 
 
-# We also obtain the mask size of every mask
+# We also obtain the shape and size of every mask
 key_nom = data[:, 5]
 key_sec = data_sec[:, 2]
+key_sec_2 = data_sec[:, 11]
 key_ext = data_ext[:, 2]
+key_ext_2 = data_ext[:, 12]
+key_ext_3 = data_ext[:, 20]
 size_nom = data[:, 6]
 size_sec = data_sec[:, 3]
+size_sec_2 = data_sec[:, 12]
 size_e = data_ext[:, 3]
+size_e_2 = data_ext[:, 13]
+size_e_3 = data_ext[:, 21]
 
+
+# Nozw all the metrics related to the COB
+delta_cob = data[:, 14]
+eta_cob = data[:, 15]
+sigma_cob = data[:, 16]
+delta_cob_sec = data_sec[:, 8]
+eta_cob_sec = data_sec[:, 9]
+sigma_cob_sec = data_sec[:, 10]
+delta_cob_sec_2 = data_sec[:, 17]
+eta_cob_sec_2 = data_sec[:, 18]
+sigma_cob_sec_2 = data_sec[:, 19]
+delta_cob_ext = data_ext[:, 9]
+eta_cob_ext = data_ext[:, 10]
+sigma_cob_ext = data_ext[:, 11]
+delta_cob_ext_2 = data_ext[:, 16]
+eta_cob_ext_2 = data_ext[:, 18]
+sigma_cob_ext_2 = data_ext[:, 19]
+delta_cob_ext_3 = data_ext[:, 25]
+eta_cob_ext_3 = data_ext[:, 26]
+sigma_cob_ext_3 = data_ext[:, 27]
 
 """
 Now we will obtain a plot showing the amount of false positives detected by Bray et al 2 x 2 mask in comparison with the
@@ -95,17 +132,30 @@ plt.show()
 """
 Now I will make a plot like Réza's to show the efficiency of both Marchiori and Extended masks method as a function
 of the magnitude of the target
-
 """
+# First the expressions for plotting the efficiency for detecting false positives for the different masks
+fp = (eta_t > flux_trsh)  # false positive
+sdr_flux = fp & (eta_c > flux_trsh) & (delta_obs_c > delta_obs_t)  # secondary mask false positive detection rate
+s_2dr_flux = fp & (eta_c_2 > flux_trsh) & (delta_obs_c_2 > delta_obs_t)  # secondary mask false positive detection rate
+edr_flux = fp & (eta_ext > flux_trsh) & (delta_obs_ext > delta_obs_t)  # extended mask false positive detection rate
+e_2dr_flux = fp & (eta_ext_2 > flux_trsh) & (delta_obs_ext_2 > delta_obs_t)  # extended (2) mask false positive detection rate
+e_3dr_flux = fp & (eta_ext_3 > flux_trsh) & (delta_obs_ext_3 > delta_obs_t)  # extended (3) mask false positive detection rate
+
 figure(0)
 clf()
 for i in range(nP):
     Pi = Pmin + i * binsize
     m = (mag >= Pi - binsize/2.) & (mag <= Pi + binsize/2.)
-    sec = ((eta_t > 7.1) & (delta_obs_c > delta_obs_t) & (eta_c > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    ext = ((eta_t > 7.1) & (delta_obs_ext > delta_obs_t) & (eta_ext > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    scatter(Pi, sec*100, color='red')
-    scatter(Pi, ext*100, color='blue')
+    sec = (sdr_flux[m].sum() / fp[m].sum()) * 100
+    sec_2 = (s_2dr_flux[m].sum() / fp[m].sum()) * 100
+    ext = (edr_flux[m].sum() / fp[m].sum()) * 100
+    ext_2 = (e_2dr_flux[m].sum() / fp[m].sum()) * 100
+    ext_3 = (e_3dr_flux[m].sum() / fp[m].sum()) * 100
+    scatter(Pi, sec, color='red')
+    scatter(Pi, sec_2, color='green')
+    scatter(Pi, ext, color='blue')
+    scatter(Pi, ext_2, color='cyan')
+    scatter(Pi, ext_3, color='magenta')
 
 xlabel("P Magnitude", fontsize=fsize)
 ylabel("Efficiency[%]", fontsize=fsize)
@@ -141,8 +191,12 @@ for i in range(nP):
     m = (mag >= Pi - binsize/2.) & (mag <= Pi + binsize/2.)
     size_nominal = np.mean(size_nom[m])
     size_ext = np.mean(size_e[m])
+    size_ext_2 = np.mean(size_e_2[m])
+    size_ext_3 = np.mean(size_e_3[m])
     scatter(Pi, size_nominal, color='black')
     scatter(Pi, size_ext, color='blue')
+    scatter(Pi, size_ext_2, color='cyan')
+    scatter(Pi, size_ext_2, color='magenta')
 
 xlabel(" P Magnitude", fontsize=fsize)
 ylabel(r"Average mask size", fontsize=fsize)
@@ -154,7 +208,10 @@ figure(3)
 clf()
 plot(mag, size_nom, 'k+', label='nominal mask')
 plot(mag, size_sec, 'r+', label='secondary mask')
+plot(mag, size_sec_2, 'g+', label='secondary mask(2)')
 plot(mag, size_e, 'b+', label='extended mask')
+plot(mag, size_e_2, 'c+', label='extended mask (2)')
+plot(mag, size_e_3, 'm+', label='extended mask (3)')
 legend()
 xlabel('P')
 ylabel(r'Mask size')
@@ -167,7 +224,9 @@ for i in range(5, 30):
     Pi = 5 + i * binsize
     m_bad = (mag_bad >= Pi - binsize/2.) & (mag_bad <= Pi + binsize/2.)
     size_secondary = np.mean(size_sec[m_bad])
+    size_secondary_2 = np.mean(size_sec_2[m_bad])
     scatter(Pi, size_secondary, color='red')
+    scatter(Pi, size_secondary_2, color='green')
 
 xlabel(" P Magnitude of the Contaminants", fontsize=fsize)
 ylabel(r"Average sec. mask size", fontsize=fsize)
@@ -185,7 +244,9 @@ for i in range(5, 30):
     Pi = 5 + i * binsize
     m_bad = (mag_bad <= Pi + binsize/2.)
     key_secondary = len(np.unique(key_sec[m_bad]))
+    key_secondary_2 = len(np.unique(key_sec_2[m_bad]))
     scatter(Pi, key_secondary, color='red')
+    scatter(Pi, key_secondary_2, color='red')
 
 xlabel("P Magnitude of the Contaminant", fontsize=fsize)
 ylabel("Cum. count of mask shapes", fontsize=fsize)
@@ -202,10 +263,16 @@ for i in range(nP):
     m = (mag <= Pi + binsize/2.)
     pix_nominal = len(np.unique(key_nom[m]))
     pix_sec = len(np.unique(key_sec[m]))
+    pix_sec_2 = len(np.unique(key_sec_2[m]))
     pix_ext = len(np.unique(key_ext[m]))
+    pix_ext_2 = len(np.unique(key_ext_2[m]))
+    pix_ext_3 = len(np.unique(key_ext_3[m]))
     scatter(Pi, pix_nominal, color='black')
     scatter(Pi, pix_sec, color='red')
+    scatter(Pi, pix_sec_2, color='green')
     scatter(Pi, pix_ext, color='blue')
+    scatter(Pi, pix_ext_2, color='cyan')
+    scatter(Pi, pix_ext_3, color='cyan')
 
 xlabel('P Magnitude', fontsize=fsize)
 ylabel('Cum. count of mask shapes', fontsize=fsize)
@@ -215,33 +282,35 @@ ylabel('Cum. count of mask shapes', fontsize=fsize)
 """
 Now let's plot the efficiency of the C.O.B. shift measurements
 """
-delta_cob = data[:, 14]
-eta_cob = data[:, 15]
-sigma_cob = data[:, 16]
-delta_cob_sec = data_sec[:, 8]
-eta_cob_sec = data_sec[:, 9]
-sigma_cob_sec = data_sec[:, 10]
-delta_cob_ext = data_ext[:, 9]
-eta_cob_ext = data_ext[:, 10]
-sigma_cob_ext = data_ext[:, 11]
+# First the expressions for computing the efficiency for detecting false positives of the cob shift for different masks
+ndr_cob = fp & (eta_cob > cob_trsh)  # nominal mask false positive detection rate via cob shift
+sdr_cob = fp & (eta_cob_sec > cob_trsh)  # secondary mask false positive detection rate via cob shift
+s_2dr_cob = fp & (eta_cob_sec_2 > cob_trsh)  # secondary mask false positive detection rate via cob shift
+edr_cob = fp & (eta_cob_ext > cob_trsh)  # extended mask false positive detection rate via cob shift
+e_2dr_cob = fp & (eta_cob_ext_2 > cob_trsh)  # extended mask (2) false positive detection rate via cob shift
+e_3dr_cob = fp & (eta_cob_ext_3 > cob_trsh)  # extended mask (3) false positive detection rate via cob shift
 
 figure(7)
 clf()
 for i in range(nP):
     Pi = Pmin + i * binsize
     m = (mag >= Pi - binsize/2.) & (mag <= Pi + binsize/2.)
-    eff_cob = ((eta_cob > 3) & (eta_t > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    eff_cob_sec = ((eta_cob_sec > 3) & (eta_t > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    eff_cob_ext = ((eta_cob_ext > 3) & (eta_t > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    scatter(Pi, eff_cob_sec * 100, color='red')
-    scatter(Pi, eff_cob*100, color='black')
-    scatter(Pi, eff_cob_ext*100, color='blue')
+    eff_cob = (ndr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_sec = (sdr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_sec_2 = (s_2dr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_ext = (edr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_ext_2 = (e_2dr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_ext_3 = (e_3dr_cob[m].sum() / fp[m].sum()) * 100
+    scatter(Pi, eff_cob_sec, color='red')
+    scatter(Pi, eff_cob_sec_2, color='green')
+    scatter(Pi, eff_cob, color='black')
+    scatter(Pi, eff_cob_ext, color='blue')
+    scatter(Pi, eff_cob_ext_2, color='cyan')
+    scatter(Pi, eff_cob_ext_2, color='magenta')
 
 #legend()
 xlabel('P Magnitude', fontsize=fsize)
 ylabel('Efficiency[%]', fontsize=fsize)
-
-#plt.show()
 
 """
 Now we plot the NSR over 1h  for every mask as a function of the target P magnitude
@@ -250,61 +319,94 @@ figure(8)
 clf()
 plot(mag, nsr1h, 'k+', label='nominal mask')
 plot(mag, nsr1h_sec, 'r+', label='secondary mask')
+plot(mag, nsr1h_sec_2, 'g+', label='secondary mask(2)')
 plot(mag, nsr1h_ext, 'b+', label='extended mask')
+plot(mag, nsr1h_ext_2, 'c+', label='extended mask (2)')
+plot(mag, nsr1h_ext_3, 'm+', label='extended mask (3)')
 semilogy()
 legend()
 xlabel('P magnitude')
 ylabel(r'$NSR_{1hr}$')
 
 """
-Now we plot the COB shift as a function of the target P magnitude
+Now we plot the statistical significance for every mask as a function of the target P magnitude
 """
 figure(9)
 clf()
+plot(mag, eta_t, 'k+', label='nominal mask')
+plot(mag, eta_c, 'r+', label='secondary mask')
+plot(mag, eta_c_2, 'g+', label='secondary mask(2)')
+plot(mag, eta_ext, 'b+', label='extended mask')
+plot(mag, eta_ext_2, 'c+', label='extended mask (2)')
+plot(mag, eta_ext_3, 'm+', label='extended mask (3)')
+semilogy()
+legend()
+xlabel('P magnitude')
+ylabel(r'$\eta$')
+
+
+"""
+Now we plot the COB shift as a function of the target P magnitude
+"""
+figure(10)
+clf()
 plot(mag, delta_cob, 'k+', label='nominal mask')
 plot(mag, delta_cob_sec, 'r+', label='secondary mask')
+plot(mag, delta_cob_sec_2, 'g+', label='secondary mask(2)')
 plot(mag, delta_cob_ext, 'b+', label='extended mask')
+plot(mag, delta_cob_ext_2, 'c+', label='extended mask(2)')
+plot(mag, delta_cob_ext_3, 'm+', label='extended mask(3)')
 xlabel('P magnitude')
 ylabel(r'$\Delta_{COB}$')
 semilogy()
 legend()
 
-figure(10)
+"""
+Now we plot the COB shift error as a function of the target P magnitude
+"""
+figure(11)
 clf()
 plot(mag, sigma_cob, 'k+', label='nominal mask')
 plot(mag, sigma_cob_sec, 'r+', label='secondary mask')
+plot(mag, sigma_cob_sec_2, 'g+', label='secondary mask(2)')
 plot(mag, sigma_cob_ext, 'b+', label='extended mask')
+plot(mag, sigma_cob_ext_2, 'c+', label='extended mask(2)')
+plot(mag, sigma_cob_ext_3, 'm+', label='extended mask(3)')
 xlabel('P magnitude')
 ylabel(r'$\sigma_{COB} [pix]$')
 semilogy()
 legend()
 
-figure(11)
-clf()
-plot(mag, sigma_cob, 'k+', label='noise')
-plot(mag, delta_cob, 'b.', label='signal')
-xlabel('P magnitude')
-ylabel(r'$\Delta_{COB}$')
-title('Nominal Mask')
-semilogy()
-legend()
 
 figure(12)
 clf()
 for i in range(nP):
     Pi = Pmin + i * binsize
     m = (mag >= Pi - binsize/2.) & (mag <= Pi + binsize/2.)
-    eff_cob = ((eta_cob > 3) & (eta_t > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    eff_cob_sec = ((eta_cob_sec > 3) & (eta_t > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    eff_cob_ext = ((eta_cob_ext > 3) & (eta_t > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    sec = ((eta_t > 7.1) & (delta_obs_c > delta_obs_t) & (eta_c > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    ext = ((eta_t > 7.1) & (delta_obs_ext > delta_obs_t) & (eta_ext > 7.1))[m].sum() / (eta_t > 7.1)[m].sum()
-    plot(Pi, sec*100, 'r+')
-    plot(Pi, ext * 100, 'b+')
-    plot(Pi, eff_cob_sec * 100, 'r^')
+    eff_cob = (ndr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_sec = (sdr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_sec_2 = (s_2dr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_ext = (edr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_ext_2 = (e_2dr_cob[m].sum() / fp[m].sum()) * 100
+    eff_cob_ext_3 = (e_3dr_cob[m].sum() / fp[m].sum()) * 100
+    sec = (sdr_flux[m].sum() / fp[m].sum()) * 100
+    ext = (edr_flux[m].sum() / fp[m].sum()) * 100
+    ext_2 = (e_2dr_flux[m].sum() / fp[m].sum()) * 100
+    ext_3 = (e_3dr_flux[m].sum() / fp[m].sum()) * 100
+    plot(Pi, sec, 'r+')
+    plot(Pi, sec_2, 'g+')
+    plot(Pi, ext, 'b+')
+    plot(Pi, ext_2, 'c+')
+    plot(Pi, ext_3, 'm+')
+    plot(Pi, eff_cob_sec, 'r^')
+    plot(Pi, eff_cob_sec_2, 'g^')
     #plot(Pi, eff_cob*100, 'k')
-    plot(Pi, eff_cob_ext*100, 'b^')
-    legend(['Sec. Mask Flux', 'Ext. Mask Flux', 'Sec. Mask COB shift', 'Ext. Mask COB shift'], loc='best')
+    plot(Pi, eff_cob_ext, 'b^')
+    plot(Pi, eff_cob_ext_2, 'c^')
+    plot(Pi, eff_cob_ext_3, 'm^')
+    legend(['Sec. Mask Flux', 'Sec. Mask Flux(2)', 'Ext. Mask Flux', 'Ext. Mask Flux (2)', 'Ext. Mask Flux (3)',
+            'Sec. Mask COB shift', 'Sec. Mask COB shift(2)', 'Ext. Mask COB shift', 'Ext. Mask COB shift (2)',
+            'Ext. Mask COB shift (3)'], loc='best')
 
 xlabel('P Magnitude', fontsize=fsize)
 ylabel('Efficiency[%]', fontsize=fsize)
