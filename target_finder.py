d095f5522bd8dbecd3d68b263cd6a028493ae502
GUTIERREZ-CANALES Fernando <fer>
2024-10-23 18:55:47 +0200

    making sure the target is above magnitude 10

--- target_finder.py
+++ target_finder.py
@@ -20,7 +20,7 @@ etx_flux_thrsh = 3
 
 # Let's get all the target IDs and magnitudes
 target_IDs = data_nominal_mask[:, 0]
-target_magnituddes = data_nominal_mask[:, 1]
+target_magnitudes = data_nominal_mask[:, 1]
 
 # Let's get all the 10-element eta_k_ext values coming from plottiing_efficiency.py
 eta_ext_flux = data_eta_extended_mask[:, :]
@@ -49,7 +49,7 @@ eta_nom_cob = data_nominal_mask[:, 46:56]
 # 4. eta_nom_cob > cob_thrsh (3)
 
 # Set a condition
-condition_set = 2  
+condition_set = 1  
 
 if condition_set == 1:
 
@@ -59,7 +59,7 @@ if condition_set == 1:
 elif condition_set == 2:
 
     # Applying the conditions element-wise and finding the indices of the matching targets
-    condition = (eta_nom_flux > flux_thrsh) & (eta_nom_cob > eta_ext_flux)  & (eta_ext_flux < etx_flux_thrsh)
+    condition = (eta_nom_flux > flux_thrsh) & (eta_nom_cob > eta_ext_flux)  & (eta_ext_flux > etx_flux_thrsh)
 
 # Now, find the target index and contaminant index where the condition holds true
 index_of_matching_targets = []
@@ -82,5 +82,7 @@ for i in range(len(index_of_matching_targets)):
     print(f"Target Index: {index_of_matching_targets[i]}, Contaminant Index: {index_of_matching_contaminants[i]}")
 
 #Let's get a target that fulfills the conditons
-good_target_ID = target_IDs[index_of_matching_targets[0]]
-good_magnitude_ID = target_magnituddes[index_of_matching_targets[0]]
+good_target_ID = target_IDs[index_of_matching_targets[1]]
+good_target_magnitude = target_magnitudes[index_of_matching_targets[1]]
+print(good_target_ID)
+print(good_target_magnitude)
