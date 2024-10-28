89712591d08fb7a23ef82263f9d489cacb5e9e3f
GUTIERREZ-CANALES Fernando <fer>
2024-10-28 16:50:55 +0100

    started to track the file that computes the efficiency for the targets across the FoV

--- dap_metrics.py
+++ dap_metrics.py
@@ -17,7 +17,6 @@ PSFfile = '/home/fercho/double-aperture-photometry/psf_flight_models_martin/PSF-
 #DIRout = 'test_results/'
 DIRout = '/home/fercho/double-aperture-photometry/simulation_results/1000_targets_per_magnnitude_bin_fixed_dback_132000ppm_and_td_1_422_hr_Noblesse_PSF/'
 
-
 # Parameters for the imagette and PSF decomposition
 size_im_x = 6  # size of the imagette (x-direction)
 size_im_y = 6  # size of the imagette (y-direction)
---
+++ efficiency_for_targets_across_FoV.py
@@ -0,0 +1,10 @@
+"""
+Script for computing the efficiency for detecting FPs
+with each method
+Fernando 28th October 2024
+"""
+
+import numpy as np              # type:ignore
+import matplotlib.pyplot as plt # type:ignore
+
+
