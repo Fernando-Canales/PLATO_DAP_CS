corrected the sign of gammax and gammay in imagette.py

imagette.py
@@ -119,13 +119,14 @@ def centroid_shift(w, Ik, n_cam, I_t, I_contaminants, sprk, dback, sb, sd, sq, t
     c_y = np.sum(y * w * I_tot) / f_tot
     # Now we define the Gamma factor along the X-direction
     #gamma_x = np.sum(x * w * Ik) / f_tot - c_x * sprk
-    gamma_x = np.sum((x - c_x)* Ikw)/ f_tot
+    gamma_x = np.sum((x + c_x)* Ikw)/ f_tot
     # Now we define the Gamma factor along the Y-direction
     #gamma_y = np.sum(y * w * Ik) / f_tot - c_y * sprk
-    gamma_y = np.sum((y - c_y)* Ikw) / f_tot
+    gamma_y = np.sum((y + c_y)* Ikw) / f_tot
     # Now we define the total gamma factor
     gamma = np.sqrt(gamma_x ** 2 + gamma_y ** 2)
     # Now we make sure to deal with the correct units for the C.O.B shift (no ppm) and define the Lambda factor
+    # Lambda =  dback*1e-6/(1 - dback*1e-6*sprk)
     Lambda = dback*1e-6 / (1 + dback*1e-6*sprk)
     # Now we define the centroid shift along the X-direction
     #cs_x = l * gamma_x