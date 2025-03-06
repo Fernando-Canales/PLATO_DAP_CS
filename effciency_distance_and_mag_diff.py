b01edbe30ef0b2e530bc00c671bfc7de6ddb1778
GUTIERREZ-CANALES Fernando <fer>
2025-03-06 11:22:05 +0100

    created the files for computing the efficiency of the current strategy and for the efficiency in several shells for the distance and mag diff

---
+++ effciency_distance_and_mag_diff.py
@@ -0,0 +1,6 @@
+"""
+Script that computes the efficiency in the
+distnacce and mag diff space
+
+Fernando 05.03.2025
+"""
---
+++ efficiency_proposed_strategy.py
@@ -0,0 +1,6 @@
+"""
+Script that creates an efficiency plots 
+for the proposed strategy to detect False Positives
+
+Fernando 05/03/2025
+"""
